3c97f8d4f7e747e2e662e5d5bb72b57ddd4a4acf
unknown <tavoywalls@gmail.com>
2022-12-12 19:45:20 -0500

    added weather api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import requests
+import requests 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,4 +7,17 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    return render_template('weather.html')
+    url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={36d5e340a45394ac57e7f479840d8fab}'
+    city = 'Las Vegas'
+
+    r = requests.get(url.format(city)).json()
+    print(r)
+
+    weather = {
+        'city' : city,
+        'temperature' : r ['main']['temp'],
+        'description' : r ['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+    }
+
+    return render_template('weather.html', weather=weather)
