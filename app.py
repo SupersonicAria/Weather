4fee96b76b016365c58915147bd82318e6ab8188
unknown <tavoywalls@gmail.com>
2022-12-12 20:06:50 -0500

    linked database

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import requests 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pink1023Heart!@localhost:5432/weather'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def index():
